bfc48215b5d40734bb9b579740c5d4bb0328a117
wyvern1423 <xiaoxun1208@126.com>
2020-04-05 20:43:04 +0800

    NumberPad sync

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -23,11 +23,13 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class NumberPad extends Vue {
-    output = '0';
+
+    @Prop() value!: number;
+    output = this.value.toString();
 
     innerContext(event: MouseEvent) {
       const button = (event.target as HTMLButtonElement);
