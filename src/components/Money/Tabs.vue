44908ba50a9c600b4681cf7e1623e2ea18fc9118
wyvern1423 <xiaoxun1208@126.com>
2020-04-10 22:12:48 +0800

    抽离Tabs.vue,使用在Statistics.vue里面

--- src/components/Money/Tabs.vue
+++ src/components/Money/Tabs.vue
@@ -1,33 +1,33 @@
 <template>
-  <div>
-    <ul class="types">
-      <li :class="{[classPrefix+'-item']:classPrefix, selected: value === '-' }" @click="selectType('-')">支出</li>
-      <li :class="{[classPrefix+'-item']:classPrefix, selected:value === '+'  }" @click="selectType('+')">收入</li>
-    </ul>
-  </div>
+  <ul class="tabs">
+    <li v-for="item in dataSource" :key="item.value"
+        :class="{selected: item.value === value ,[classPrefix + '-tabs-item']:classPrefix}"
+        @click="select(item)">
+      {{item.text}}
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  type dataSourceItem = { text: string; value: string };
 
   @Component
-  export default class Types extends Vue {
+  export default class Tabs extends Vue {
+    @Prop({required: true, type: Array}) dataSource!: dataSourceItem[];
     @Prop(String) readonly value!: string;
     @Prop(String) classPrefix?: string;
 
-    selectType(type: string) {
-      if (type !== '-' && type !== '+') {
-        throw new Error('type is unknown');
-      }
-      this.$emit('update:value', type);
+    select(item: dataSourceItem) {
+      this.$emit('update:value', item.value);
     }
   }
-
 </script>
 
 <style lang="scss" scoped>
-  .types {
+  .tabs {
     background: #c4c4c4;
     display: flex;
     text-align: center;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,17 +1,45 @@
 <template>
-  <div>
-    <Layout>
-      <p>statistics</p>
-    </Layout>
-  </div>
+  <Layout>
+    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
+    <Tabs :data-source="intervalList" :value.sync="interval"/>
+  </Layout>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Statistics.vue',
-  };
+  import Tabs from '@/components/Money/Tabs.vue';
+  import {Component} from 'vue-property-decorator';
+  import Vue from 'vue';
+  import Types from '@/components/Money/Types.vue';
+
+  @Component({
+    components: {Tabs, Types},
+  })
+  export default class Statistics extends Vue {
+    type = '-';
+    interval = 'day';
+    intervalList = [
+      {text: '按天', value: 'day'},
+      {text: '按周', value: 'week'},
+      {text: '按月', value: 'month'}
+    ];
+    typeList = [
+      {text: '支出', value: '-'},
+      {text: '收入', value: '+'}
+    ];
+  }
 </script>
 
 <style lang="scss" scoped>
+  ::v-deep .type-tabs-item {
+    background: white;
+
+    &.selected {
+      background: #c4c4c4;
+
+      &::after {
+        display: none;
+      }
+    }
+  }
 
 </style>
