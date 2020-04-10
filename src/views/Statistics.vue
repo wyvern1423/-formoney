bec02aba460b6c5eab057a0c378d67bd9aaa03c8
wyvern1423 <xiaoxun1208@126.com>
2020-04-11 00:18:29 +0800

    更新css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <Tabs :data-source="intervalList" :value.sync="interval"/>
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
   </Layout>
 </template>
 
@@ -34,6 +34,9 @@
         display: none;
       }
     }
+  }
+  ::v-deep .interval-tabs-item{
+    height: 48px;
   }
 
 </style>
